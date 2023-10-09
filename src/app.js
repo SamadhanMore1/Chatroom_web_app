5a98164f16fd4bf0eb3844d6f1191c170a9e0fb2
SamadhanMore1 <131381363+SamadhanMore1@users.noreply.github.com>
2023-10-09 09:13:44 +0530

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+let express = require( 'express' );
+let app = express();
+let server = require( 'http' ).Server( app );
+let io = require( 'socket.io' )( server );
+let stream = require( './ws/stream' );
+let path = require( 'path' );
+let favicon = require( 'serve-favicon' );
+let port = process.env.PORT || 8000;
+
+app.use( favicon( path.join( __dirname, 'favicon.png' ) ) );
+app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
+
+app.get( '/', ( req, res ) => {
+    res.sendFile( __dirname + '/index.html' );
+} );
+
+
+io.of( '/stream' ).on( 'connection', stream );
+
+server.listen( port , () =>{
+    console.log("server listening on port " + port)
+});
